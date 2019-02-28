8972b8799c6a0192588b48a310794ebb7f1201e0
joelcorey <joelcorey@fastmail.com>
2019-02-28 08:40:10 -0800

    factory function add

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,6 +1,22 @@
 'use strict';
 
 const Item = (function () { 
+  const validateName = function(name) {
+    if(!name){
+      throw new TypeError('Name does not exist');
+    }
+  };
 
-  return {};
+  const create = function(name) {
+    return {
+      id: cuid(),
+      name,
+      checked: false,
+    };
+  };
+
+  return {
+    validateName,
+    create,
+  };
 }());
